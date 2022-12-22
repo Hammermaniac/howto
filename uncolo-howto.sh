6605ccb75eb70cbfe31b6c545c62c314af0f1614
Eswee Louw <esweelouw123@gmail.com>
2022-12-23 00:08:15 +1300

    Spacing edit.

--- uncolo-howto.sh
+++ uncolo-howto.sh
@@ -6,5 +6,6 @@ while IFS= read -r line
   do
 	echo -e "$line" >> ~/personal/howto/howtos.bak
 done < ~/personal/howto/howtos
+
 sed -i 's,\x1B\[[0-9;]*[a-zA-Z],,g' ~/personal/howto/howtos.bak
 sed -i '1,9d' ~/personal/howto/howtos.bak
