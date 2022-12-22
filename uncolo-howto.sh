a56edba554ea9bd9b30f33ce85dc8407f39e7888
Eswee Louw <esweelouw123@gmail.com>
2022-12-23 00:07:36 +1300

    Added non-color script.

---
+++ uncolo-howto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat /dev/null > ~/personal/howto/howtos.bak
+
+while IFS= read -r line
+  do
+	echo -e "$line" >> ~/personal/howto/howtos.bak
+done < ~/personal/howto/howtos
+sed -i 's,\x1B\[[0-9;]*[a-zA-Z],,g' ~/personal/howto/howtos.bak
+sed -i '1,9d' ~/personal/howto/howtos.bak
