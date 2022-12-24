16e493838fee8bd453adbbed832637630ad2eb46
Eswee Louw <esweelouw123@gmail.com>
2022-12-25 03:40:39 +1300

    Somehow fixed uncolor.sh using -e option with echo...

--- uncolo-howto.sh
+++ uncolo-howto.sh
@@ -4,8 +4,9 @@ cat /dev/null > ~/personal/howto/howtos.bak
 
 while IFS= read -r line
   do
-	echo -e "$line" >> ~/personal/howto/howtos.bak
-done < ~/personal/howto/howtos
+	echo -n "$line" >> ~/personal/howto/howtos.bak
+	echo '' >> ~/personal/howto/howtos.bak
+done < ~/.common-configs/howto.main
 
 sed -i 's,\x1B\[[0-9;]*[a-zA-Z],,g' ~/personal/howto/howtos.bak
-sed -i '1,9d' ~/personal/howto/howtos.bak
+#sed -i '1,9d' ~/personal/howto/howtos.bak
