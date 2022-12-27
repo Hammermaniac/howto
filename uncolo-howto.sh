ffde4ed73fb09422bb803781c282ce00e1bd52af
Eswee Louw <esweelouw123@gmail.com>
2022-12-28 12:37:16 +1300

    howto book updates.

--- uncolo-howto.sh
+++ uncolo-howto.sh
@@ -4,9 +4,11 @@ cat /dev/null > ~/personal/howto/howtos.bak
 
 while IFS= read -r line
   do
-	echo -n "$line" >> ~/personal/howto/howtos.bak
-	echo '' >> ~/personal/howto/howtos.bak
+#	printf "%s\n" "$line" >> ~/personal/howto/howtos.bak
+	line=$(echo "$line" | sed 's,`printf "%s\033" "\033\[[0-9;]+m"`,,')
+	echo "$line" >> ~/personal/howto/howtos.bak
+#	echo '' >> ~/personal/howto/howtos.bak
 done < ~/.common-configs/howto.main
 
-sed -i 's,\x1B\[[0-9;]*[a-zA-Z],,g' ~/personal/howto/howtos.bak
+#sed -i 's,\x1B\[[0-9;]*[a-zA-Z],,g' ~/personal/howto/howtos.bak
 #sed -i '1,9d' ~/personal/howto/howtos.bak
